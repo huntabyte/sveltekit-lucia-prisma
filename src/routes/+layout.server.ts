28b1cb60f1fd546eddd98ddb11d8ff4905d36cfb
Hunter Johnston <johnstonhuntera@gmail.com>
2023-02-05 13:22:35 -0500

    video-starter

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,3 @@
+import type { LayoutServerLoad } from "./$types"
+
+export const load: LayoutServerLoad = async () => {}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
 		<h3>New Article</h3>
 		<label for="title"> Title </label>
 		<input type="text" id="title" name="title" />
-		<label for="title"> Title </label>
+		<label for="title"> Content </label>
 		<textarea id="content" name="content" rows={5} />
 		<button type="submit">Add Article</button>
 	</form>
